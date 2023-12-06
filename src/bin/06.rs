2c6dfc3500663d2645dbc14363da6181f12a0c7d
nyuriumuri <nyuriumuri@gmail.com>
2023-12-06 09:36:38 +0200

    fixed test error in day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -53,11 +53,24 @@ struct Race {
 impl Race {
     fn solve(&self) -> u64 {
         // quadratic formula. a=-1, b=time, c=-distance
+
         let b = self.time as f32;
         let c = -(self.distance as f32);
         let root = f32::sqrt(b.powf(2f32) + 4f32 * c);
-        let x1 = ((root - b) / -2f32).ceil() as u64;
-        let x2 = ((-root - b) / -2f32).floor() as u64;
+        let x1 = (root - b) / -2f32;
+        let x2 = (-root - b) / -2f32;
+
+        let x1 = if x1.fract() == 0f32 {
+            x1 as u64 + 1
+        } else {
+            x1.ceil() as u64
+        };
+
+        let x2 = if x2.fract() == 0f32 {
+            x2 as u64 - 1
+        } else {
+            x2.floor() as u64
+        };
 
         x2 - x1 + 1
     }
