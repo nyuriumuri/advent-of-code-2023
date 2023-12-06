aace9e8ff58088aa8d26782c2b170ffe2eb8fef1
nyuriumuri <nyuriumuri@gmail.com>
2023-12-06 09:08:24 +0200

    trimming day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -6,15 +6,15 @@ use regex::Regex;
 lazy_static! {
     static ref DIGIT_RE: Regex = Regex::new(r"\d+").unwrap();
 }
-pub fn part_one(input: &str) -> Option<usize> {
+pub fn part_one(input: &str) -> Option<u64> {
     let (time_str, dist_str) = input.split_once('\n').unwrap();
-    let res: usize = DIGIT_RE
+    let res: u64 = DIGIT_RE
         .captures_iter(time_str)
-        .map(|c| c[0].parse::<usize>().unwrap())
+        .map(|c| c[0].parse::<u64>().unwrap())
         .zip(
             DIGIT_RE
                 .captures_iter(dist_str)
-                .map(|c| c[0].parse::<usize>().unwrap()),
+                .map(|c| c[0].parse::<u64>().unwrap()),
         )
         .map(|pair| Race {
             time: pair.0,
@@ -25,20 +25,20 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(res)
 }
 
-pub fn part_two(input: &str) -> Option<usize> {
+pub fn part_two(input: &str) -> Option<u64> {
     let (time_str, dist_str) = input.split_once('\n').unwrap();
     let time = time_str
         .chars()
         .filter(|c| c.is_ascii_digit())
         .collect::<String>()
-        .parse::<usize>()
+        .parse::<u64>()
         .unwrap();
 
     let distance = dist_str
         .chars()
         .filter(|c| c.is_ascii_digit())
         .collect::<String>()
-        .parse::<usize>()
+        .parse::<u64>()
         .unwrap();
 
     Some(Race { time, distance }.solve())
@@ -46,30 +46,18 @@ pub fn part_two(input: &str) -> Option<usize> {
 
 #[derive(Debug, Clone, Copy)]
 struct Race {
-    time: usize,
-    distance: usize,
+    time: u64,
+    distance: u64,
 }
 
 impl Race {
-    fn solve(&self) -> usize {
-        // quadratic formula. a=1, b=time, c=-distance
+    fn solve(&self) -> u64 {
+        // quadratic formula. a=-1, b=time, c=-distance
         let b = self.time as f32;
         let c = -(self.distance as f32);
         let root = f32::sqrt(b.powf(2f32) + 4f32 * c);
-        let x1 = (root - b) / -2f32;
-        let x2 = (-root - b) / -2f32;
-
-        let x1 = if x1.fract() == 0f32 {
-            x1 as usize + 1
-        } else {
-            x1.ceil() as usize
-        };
-
-        let x2 = if x2.fract() == 0f32 {
-            x2 as usize - 1
-        } else {
-            x2.floor() as usize
-        };
+        let x1 = ((root - b) / -2f32).ceil() as u64;
+        let x2 = ((-root - b) / -2f32).floor() as u64;
 
         x2 - x1 + 1
     }
