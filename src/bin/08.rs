f7dde3d0532bda5dce718764c52abc08d0eb06f3
nyuriumuri <nyuriumuri@gmail.com>
2023-12-08 13:37:46 +0200

    solved day 8 part 2 with lcm

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -2,13 +2,14 @@ advent_of_code::solution!(8);
 use std::collections::HashMap;
 
 use lazy_static::lazy_static;
+use rayon::prelude::*;
 use regex::Regex;
 
 lazy_static! {
     static ref MAP_RE: Regex =
         Regex::new(r"(?<Location>\w+)\s*=\s*\((?<Left>\w+)\s*,\s*(?<Right>\w+)\)").unwrap();
 }
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let (instructions, map_str) = input.split_once('\n').unwrap();
     let instructions = instructions.trim();
     let map: HashMap<String, (String, String)> = MAP_RE
@@ -35,12 +36,12 @@ pub fn part_one(input: &str) -> Option<u32> {
             cur = next;
             cur != zzz
         })
-        .count() as u32
+        .count() as u64
         + 1;
     Some(steps)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u64> {
     let (instructions, map_str) = input.split_once('\n').unwrap();
     let instructions = instructions.trim();
     let map: HashMap<String, (String, String)> = MAP_RE
@@ -53,35 +54,44 @@ pub fn part_two(input: &str) -> Option<u32> {
         })
         .collect();
 
-    let mut starting: Vec<String> = map
+    let res = map
         .keys()
         .filter(|key| key.ends_with('A'))
-        .cloned()
-        .collect();
-
-    let steps = instructions
-        .chars()
-        .cycle()
-        .take_while(|&dir| {
-            let prev = starting.clone();
-            starting.iter_mut().for_each(|cur| {
-                *cur = match dir {
-                    'R' => map.get(cur).unwrap().1.clone(),
-                    'L' => map.get(cur).unwrap().0.clone(),
-                    _ => unimplemented!(),
-                };
-            });
-            if prev == starting {
-                panic!("Vector didn't change between iterations!")
-            }
-            starting.iter().map(|cur| cur.ends_with('Z')).any(|b| !b)
+        .par_bridge()
+        .map(|start: &String| {
+            let mut cur = start.clone();
+            let cloned_map = map.clone();
+            instructions
+                .chars()
+                .cycle()
+                .take_while(move |dir| {
+                    cur = match dir {
+                        'R' => cloned_map.get(&cur).unwrap().1.clone(),
+                        'L' => cloned_map.get(&cur).unwrap().0.clone(),
+                        _ => unimplemented!(),
+                    };
+                    !cur.ends_with('Z')
+                })
+                .count() as u64
+                + 1
         })
-        .count() as u32
-        + 1;
-    Some(steps)
+        .reduce(|| 1, lcm);
+
+    Some(res)
     // None
 }
 
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    a * b / gcd(a, b)
+}
 #[cfg(test)]
 mod tests {
     use std::{env, fs};
@@ -100,6 +110,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
